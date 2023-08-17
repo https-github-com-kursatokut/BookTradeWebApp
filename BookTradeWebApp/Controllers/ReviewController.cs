4c07d8ea85419fafc91a2e0564900d910c46b3b1
muhammetdulek <muhammetdulek56@hotmail.com>
2023-08-17 14:13:43 +0300

    Controller Ekleme2

---
+++ BookTradeWebApp/Controllers/ReviewController.cs
@@ -0,0 +1,100 @@
+﻿using BookTradeWebApp.DataContexts;
+using BookTradeWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookTradeWebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReviewController : ControllerBase
+    {
+        private readonly UserDbContext _context;
+
+        public ReviewController(UserDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Review>>> GetAllReviews()
+        {
+            return await _context.Reviews.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Review>> GetReviewById(int id)
+        {
+            var review = await _context.Reviews.FindAsync(id);
+
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return review;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Review>> CreateReview(Review review)
+        {
+            _context.Reviews.Add(review);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetReviewById), new { id = review.ReviewId }, review);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateReview(int id, Review review)
+        {
+            if (id != review.ReviewId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(review).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReviewExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteReview(int id)
+        {
+            var review = await _context.Reviews.FindAsync(id);
+
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            _context.Reviews.Remove(review);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ReviewExists(int id)
+        {
+            return _context.Reviews.Any(r => r.ReviewId == id);
+        }
+    }
+}
---
+++ BookTradeWebApp/Controllers/TradeOfferController.cs
@@ -0,0 +1,100 @@
+﻿using BookTradeWebApp.DataContexts;
+using BookTradeWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookTradeWebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TradeOfferController : ControllerBase
+    {
+        private readonly UserDbContext _context;
+
+        public TradeOfferController(UserDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TradeOffer>>> GetAllTradeOffers()
+        {
+            return await _context.TradeOffers.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TradeOffer>> GetTradeOfferById(int id)
+        {
+            var tradeOffer = await _context.TradeOffers.FindAsync(id);
+
+            if (tradeOffer == null)
+            {
+                return NotFound();
+            }
+
+            return tradeOffer;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TradeOffer>> CreateTradeOffer(TradeOffer tradeOffer)
+        {
+            _context.TradeOffers.Add(tradeOffer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTradeOfferById), new { id = tradeOffer.TradeOfferId }, tradeOffer);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTradeOffer(int id, TradeOffer tradeOffer)
+        {
+            if (id != tradeOffer.TradeOfferId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tradeOffer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TradeOfferExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTradeOffer(int id)
+        {
+            var tradeOffer = await _context.TradeOffers.FindAsync(id);
+
+            if (tradeOffer == null)
+            {
+                return NotFound();
+            }
+
+            _context.TradeOffers.Remove(tradeOffer);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TradeOfferExists(int id)
+        {
+            return _context.TradeOffers.Any(to => to.TradeOfferId == id);
+        }
+    }
+}
